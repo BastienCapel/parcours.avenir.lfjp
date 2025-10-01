b7fce95aaa463930af7cd1a8e23bfce49a00e0ef
CAPEL Bastien <capel.direction@gmail.com>
2025-10-01 20:08:37 +0000

    Improve text normalization and rebuild bundle

--- scripts/utils/stringUtils.js
+++ scripts/utils/stringUtils.js
@@ -4,7 +4,14 @@ export function classNames(...values) {
 
 export function normalizeText(value) {
   return (value || "")
+    .replace(/[\u00A0\u202F]/g, " ")
+    .replace(/[\u2010-\u2015]/g, "-")
+    .replace(/[\u2018\u2019\u201B]/g, "'")
+    .replace(/[\u201C\u201D]/g, '"')
+    .replace(/-/g, " ")
+    .replace(/\s{2,}/g, " ")
     .toLowerCase()
     .normalize("NFD")
-    .replace(/\p{Diacritic}/gu, "");
+    .replace(/\p{Diacritic}/gu, "")
+    .trim();
 }
